f93d8c7425273351026dbf7a79d7fa43a0ae810d
Cezar Bulancea <118464357+Cezarrr9@users.noreply.github.com>
2024-02-07 11:26:58 +0100

    q learning agent

---
+++ src/agents/ql.py
@@ -0,0 +1,54 @@
+import numpy as np
+from collections import defaultdict
+import gymnasium as gym
+
+class QLAgent:
+
+    def __init__(self,
+                 env: gym.wrappers,
+                 learning_rate: float, 
+                 discount_factor: float, 
+                 epsilon_start: float,
+                 epsilon_end: float, 
+                 epsilon_decay: float) -> None:
+        
+        self.env = env
+
+        self.q_values = defaultdict(lambda: np.zeros(self.env.action_spaces.n))
+
+        self.learning_rate = learning_rate
+        self.discount_factor = discount_factor
+        
+        self.epsilon = epsilon_start
+        self.epsilon_end = epsilon_end
+        self.epsilon_decay = epsilon_decay
+
+        self.training_error = []
+
+    def select_action(self, obs: np.ndarray[float, float, float, float]) -> int:
+
+        if np.random.random() < self.epsilon:
+            return self.env.action_space.sample()
+        
+        else:
+            return int(np.argmax(self.q_values[obs]))
+        
+    def update(self,
+               obs: np.ndarray[float, float, float, float],
+               action: int,
+               reward: int,
+               terminated: bool,
+               next_obs: np.ndarray[float, float, float, float]) -> None:
+        
+        future_q_value = (not terminated) * np.max(self.q_values[next_obs])
+        temporal_difference = reward + self.discount_factor * future_q_value - self.q_values[obs][action]
+
+        self.q_values[obs][action] += self.learning_rate * temporal_difference
+
+        self.training_error.append(temporal_difference)
+
+    def decay_epsilon(self) -> None:
+        self.epsilon = max(self.epsilon_end, self.epsilon - self.epsilon_decay)
+
+
+        

f28563d95edbd87e9dca292a2d5b8b0305cf2152
Cezar Bulancea <118464357+Cezarrr9@users.noreply.github.com>
2024-02-08 14:07:25 +0100

    implement some plotting but it doesn t learn yet

--- src/agents/ql.py
+++ src/agents/ql.py
@@ -5,16 +5,16 @@ import gymnasium as gym
 class QLAgent:
 
     def __init__(self,
-                 env: gym.wrappers,
+                 action_space,
                  learning_rate: float, 
                  discount_factor: float, 
                  epsilon_start: float,
                  epsilon_end: float, 
                  epsilon_decay: float) -> None:
         
-        self.env = env
+        self.action_space = action_space
 
-        self.q_values = defaultdict(lambda: np.zeros(self.env.action_spaces.n))
+        self.q_values = defaultdict(lambda: np.zeros(action_space.n))
 
         self.learning_rate = learning_rate
         self.discount_factor = discount_factor
@@ -25,25 +25,25 @@ class QLAgent:
 
         self.training_error = []
 
-    def select_action(self, obs: np.ndarray[float, float, float, float]) -> int:
+    def select_action(self, obs: np.ndarray) -> int:
 
         if np.random.random() < self.epsilon:
-            return self.env.action_space.sample()
+            return self.action_space.sample()
         
         else:
-            return int(np.argmax(self.q_values[obs]))
+            return int(np.argmax(self.q_values[tuple(obs)]))
         
     def update(self,
-               obs: np.ndarray[float, float, float, float],
+               obs: np.ndarray,
                action: int,
                reward: int,
                terminated: bool,
-               next_obs: np.ndarray[float, float, float, float]) -> None:
-        
-        future_q_value = (not terminated) * np.max(self.q_values[next_obs])
-        temporal_difference = reward + self.discount_factor * future_q_value - self.q_values[obs][action]
+               next_obs: np.ndarray) -> None:
 
-        self.q_values[obs][action] += self.learning_rate * temporal_difference
+        future_q_value = (not terminated) * np.max(self.q_values[tuple(next_obs)])
+        temporal_difference = reward + self.discount_factor * future_q_value - self.q_values[tuple(obs)][action]
+
+        self.q_values[tuple(obs)][action] += self.learning_rate * temporal_difference
 
         self.training_error.append(temporal_difference)
 

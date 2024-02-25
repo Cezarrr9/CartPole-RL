1223bb7e2b9f165be35fa386752aef84f38fe73a
Cezar Bulancea <118464357+Cezarrr9@users.noreply.github.com>
2024-02-26 00:56:34 +0100

    improved again comments q learning

--- src/agents/ql.py
+++ src/agents/ql.py
@@ -30,12 +30,12 @@ def bucketize(obs: np.ndarray, obs_bounds: list) -> tuple:
     number of buckets.
 
     Parameters:
-    - obs (np.ndarray): the continuous observation given by the environment
-    - obs_bounds (list[tuple]): the accepted boundaries of the observation space for each type
-    of information (check https://gymnasium.farama.org/environments/classic_control/cart_pole/#observation-space)
+    - obs (np.ndarray): The continuous observation given by the environment.
+    - obs_bounds (list[tuple]): The accepted boundaries of the observation space for each type
+    of information (check https://gymnasium.farama.org/environments/classic_control/cart_pole/#observation-space).
 
     Returns:
-    - bucket_indices (tuple[int]): the indices of the buckets for each type of information
+    - bucket_indices (tuple[int]): The indices of the buckets for each type of information.
     
     """
     # Define the number of buckets for each type of observation.
@@ -82,13 +82,13 @@ class QLAgent:
     """Q-learning Agent
     
     Attributes:
-    - n_actions (int): the number of actions available for the agent
-    - q_values (defaultdict[np.ndarray]): the current state action values 
-    - learning_rate (float): the value of the current learning rate
-    - min_learning_rate (float): the minimum value that the learning rate can take 
-    - discount_factor (float): the discount factor used in the q-learning algorithm 
-    - epsilon (float): the value of the current epsilon threshold
-    - min_epsilon (float): the minimum value that the epsilon threshold can take 
+    - n_actions (int): The number of actions available for the agent.
+    - q_values (defaultdict[np.ndarray]): The current state action values.
+    - learning_rate (float): The value of the current learning rate.
+    - min_learning_rate (float): The minimum value that the learning rate can take.
+    - discount_factor (float): The discount factor used in the q-learning algorithm. 
+    - epsilon (float): The value of the current epsilon threshold.
+    - min_epsilon (float): The minimum value that the epsilon threshold can take.
     
     Methods:
     - select_action(state): Selects an action using the epsilon greedy policy.
@@ -116,13 +116,13 @@ class QLAgent:
         self.min_epsilon = min_epsilon
 
     def select_action(self, state: tuple[float, float, float, float]) -> int:
-        """Selects an action using the epsilon greedy policy
+        """Selects an action using the epsilon greedy policy.
         
         Parameters:
-        - state (tuple[float]): the discretized version of the state
+        - state (tuple[float]): The discretized version of the state.
 
         Returns:
-        - int: the index of an action
+        - (int): The index of an action.
         """
 
         # If the random selected number is bigger than epsilon, then select
@@ -143,11 +143,11 @@ class QLAgent:
         """ Updates the state action values.
 
         Parameters:
-        - state (tuple[float]): the discretized version of the current state
-        - action (int): the index of the selected action
-        - reward (int): the reward obtained after performing the action in the current state
-        - terminated (bool): a flag that indicates if the episode was terminated
-        - next_state (tuple[float]): the discretized version of the next state
+        - state (tuple[float]): The discretized version of the current state.
+        - action (int): The index of the selected action.
+        - reward (int): The reward obtained after performing the action in the current state.
+        - terminated (bool): A flag that indicates if the episode was terminated.
+        - next_state (tuple[float]): The discretized version of the next state.
         
         """
 
@@ -161,7 +161,7 @@ class QLAgent:
         """Decreases the value of the epsilon threshold over time.
         
         Parameters:
-        - step (int): the number of steps passed until the current moment
+        - step (int): The number of steps passed until the current moment.
         """
         self.epsilon =  max(self.min_epsilon, min(1.0, 1.0 - math.log10((step + 1) / 25)))
     
@@ -169,7 +169,7 @@ class QLAgent:
         """Decreases the value of the learning rate over time.
         
         Parameters:
-        - step (int): the number of steps passed until the current moment
+        - step (int): The number of steps passed until the current moment.
         """
         self.learning_rate =  max(self.min_learning_rate, min(1.0, 1.0 - math.log10((step + 1) / 25)))
 
@@ -177,12 +177,12 @@ class QLAgent:
         """Train the agent using the Q-learning algorithm.
         
         Paraemeters:
-        - env (gym.wrappers): the environment
-        - num_episodes (int): the number of episodes for which the agent is trained
+        - env (gym.wrappers): The environment where the agent is trained. 
+        - num_episodes (int): The number of episodes for which the agent is trained.
 
         Returns:
-        - episode_durations (list[int]): the number of timesteps each episode lasted (the same as
-        the reward in the CartPole-v1 environment)
+        - episode_durations (list[int]): The number of timesteps each episode lasted (the same as
+        the reward in the CartPole-v1 environment).
         
         """
         episode_durations = []
